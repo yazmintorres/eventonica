9d51c42fcc6986b62b76cac8cb403690c87c7c19
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-13 15:38:39 -0700

    initial setup

---
+++ client/src/components/events.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import EventCard from "./event";
+import CardGroup from 'react-bootstrap/CardGroup';
+
+
+
+function Events() {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/events")
+          .then((response) => response.json())
+          .then(events => {
+            setEvents(events); 
+            console.log('Events fetched...', events);
+            });
+          }, []);
+
+  return (
+    <CardGroup className="Events">
+            {events.map(event =>
+            <EventCard key={event.id} title={event.title} location={event.location} time={event.eventtime}/>
+            )}
+    </CardGroup>
+  );
+}
+
+export default Events;
