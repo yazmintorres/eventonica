2790759d50c030875e8ae54f6299912e9d09f118
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-14 23:51:42 -0700

    Create a component for top panel

---
+++ client/src/components/TopPanel.js
@@ -0,0 +1,10 @@
+const TopPanel = () => {
+  return (
+    <div className="top-panel">
+      <button className="btn btn-panel btn-add">Add Event</button>
+      <button className="btn btn-panel btn-fav">Filter Favorite</button>
+    </div>
+  );
+};
+
+export { TopPanel };
