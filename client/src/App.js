2464d941a0ac98a0f6567e7ccd93776abfe084ff
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-14 23:52:21 -0700

    Create header and top panel for front-end layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import Events from './components/events';
+import "./styles.css";
+import { TopPanel } from "./components/TopPanel";
+import Events from "./components/events";
 
 function App() {
   return (
     <div className="App">
-      <h1>Techtonica 2023 events</h1>
-    <Events />
+      <header>
+        <h1>Techtonica Events 2023</h1>
+      </header>
+      <main>
+        <TopPanel />
+        <Events />
+      </main>
     </div>
   );
 }
