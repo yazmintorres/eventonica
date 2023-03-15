e63770b4ad8386d1ac1f031c8f67b69e5a771cd9
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-14 23:51:57 -0700

    Reformatted using prettier

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-//This is the minimal express server. 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-const db = require('../server/db/db-connection.js'); 
+//This is the minimal express server.
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+const db = require("../server/db/db-connection.js");
 
 const app = express();
 const PORT = 8080;
@@ -18,36 +18,33 @@ app.use(bodyParser.json());
 
 // //creates an endpoint for the route `/`
 app.get("/", (req, res) => {
-    res.json("Hello Techtonica Server for an app with Events");
-  });
+  res.json("Hello Techtonica Server for an app with Events");
+});
 
+app.get("/api/events", async (req, res) => {
+  //real connection with the DB eventonica
+  try {
+    // renaming rows to events
+    const { rows: events } = await db.query("SELECT * FROM events");
+    res.send(events);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error });
+  }
 
-app.get('/api/events', async (req, res) =>{
+  //hardcode the events response for testing reasons. This call has one more event that the real DB
+  // const events = [
 
-    //real connection with the DB eventonica
-    try{
-        const { rows: events } = await db.query('SELECT * FROM events');
-        res.send(events);
+  //     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
+  //     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
+  //     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
+  //     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
+  //     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
+  //     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
+  //   ];
+  // res.json(events);
+});
 
-    } catch(error){
-        console.log(error);
-        return res.status(400).json({error});
-
-    }
-
-    //hardcode the events response for testing reasons. This call has one more event that the real DB 
-    // const events = [
-
-    //     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
-    //     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
-    //     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
-    //     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
-    //     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
-    //     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
-    //   ];
-    // res.json(events);
-})
-
-
-
-app.listen(PORT, () => console.log(`Hola! Server running on Port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Hola! Server running on Port http://localhost:${PORT}`)
+);
