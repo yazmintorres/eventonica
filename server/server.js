4b9421d9856b390e4e480faacb3fd5aa78a9c460
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-15 15:43:38 -0700

    Update database

--- server/server.js
+++ server/server.js
@@ -16,35 +16,29 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// //creates an endpoint for the route `/`
-app.get("/", (req, res) => {
-  res.json("Hello Techtonica Server for an app with Events");
-});
-
+// get all events
 app.get("/api/events", async (req, res) => {
-  //real connection with the DB eventonica
   try {
-    // renaming rows to events
-    const { rows: events } = await db.query("SELECT * FROM events");
-    res.send(events);
+    // get from database
+    const allEvents = await db.query("SELECT * FROM events");
+    res.json(allEvents.rows);
   } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error });
+    console.error(error.message);
   }
-
-  //hardcode the events response for testing reasons. This call has one more event that the real DB
-  // const events = [
-
-  //     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
-  //     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
-  //     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
-  //     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
-  //     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
-  //     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
-  //   ];
-  // res.json(events);
 });
 
 app.listen(PORT, () =>
   console.log(`Hola! Server running on Port http://localhost:${PORT}`)
 );
+
+//hardcode the events response for testing reasons. This call has one more event that the real DB
+// const events = [
+
+//     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
+//     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
+//     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
+//     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
+//     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
+//     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
+//   ];
+// res.json(events);
